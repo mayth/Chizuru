f5fd2ad3ff66512f043f2b5d70fee3305b309ad7
mayth <chimeaquas@hotmail.com>
2013-05-23 14:55:23 +0900

    Modify test code to use Chizuru structure.

--- test.rb
+++ test.rb
@@ -1,6 +1,30 @@
+require './source'
+require './provider'
+require './consumer'
+require './deliverer'
 require './user_stream'
 
-us = Chizuru::UserStream.new('credential.yaml', 'meitanbot', 'cert', 'meitanbot/2.0 (Chizuru/1.0)')
-us.connect do |json|
-  p json
+class SimpleConsumer < Chizuru::Consumer
+  def receive(data)
+    p data
+  end
 end
+
+class LoggingConsumer < Chizuru::Consumer
+  def initialize(log_file)
+    @log_file = log_file
+  end
+
+  def receive(data)
+    open(@log_file, 'a:utf-8') do |file|
+      file.puts(data.inspect)
+    end
+  end
+end
+
+provider = Chizuru::Provider.new
+provider.add_consumer(SimpleConsumer.new)
+provider.add_consumer(LoggingConsumer.new('./stream.log'))
+source = Chizuru::UserStream.new(provider, 'credential.yaml', 'meitanbot', 'cert', 'meitanbot/2.0 (Chizuru/1.0)')
+
+source.start
