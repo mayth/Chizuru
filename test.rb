dfafa4932d0b05ff668fda5ffda543872e12413e
mayth <chimeaquas@hotmail.com>
2013-05-24 14:52:47 +0900

    Modify test code for recent changes

--- test.rb
+++ test.rb
@@ -1,21 +1,26 @@
-require './source'
 require './provider'
 require './consumer'
-require './deliverer'
+require './credential'
 require './user_stream'
 
 class SimpleConsumer < Chizuru::Consumer
   def receive(data)
+    deliver(data)
+  end
+end
+
+class EchoDeliverer
+  def deliver(data)
     p data
   end
 end
 
-class LoggingConsumer < Chizuru::Consumer
+class LoggingDeliverer
   def initialize(log_file)
     @log_file = log_file
   end
 
-  def receive(data)
+  def deliver(data)
     open(@log_file, 'a:utf-8') do |file|
       file.puts(data.inspect)
     end
@@ -23,8 +28,11 @@ class LoggingConsumer < Chizuru::Consumer
 end
 
 provider = Chizuru::Provider.new
-provider.add_consumer(SimpleConsumer.new)
-provider.add_consumer(LoggingConsumer.new('./stream.log'))
-source = Chizuru::UserStream.new(provider, 'credential.yaml', 'meitanbot', 'cert', 'meitanbot/2.0 (Chizuru/1.0)')
+consumer = SimpleConsumer.new
+consumer.add_deliverer(EchoDeliverer.new)
+consumer.add_deliverer(LoggingDeliverer.new('./stream.log'))
+provider.add_consumer(consumer)
+credential = Chizuru::Credential.new('credential.yaml')
+source = Chizuru::UserStream.new(provider, credential, 'meitanbot', 'cert', 'meitanbot/2.0 (Chizuru/1.0)')
 
 source.start
