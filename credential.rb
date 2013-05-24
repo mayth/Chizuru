b8be353f65eeeea9d1d896d473b91f6d61ea59d7
mayth <chimeaquas@hotmail.com>
2013-05-24 14:48:36 +0900

    Add a class that contains credentials

---
+++ credential.rb
@@ -0,0 +1,18 @@
+require 'yaml'
+
+module Chizuru
+  class Credential
+    attr_reader :consumer, :access_token
+    def initialize(path)
+      cred = YAML.load_file(path)
+      @consumer = OAuth::Consumer.new(
+        cred['consumer_key'],
+        cred['consumer_secret'],
+        site: 'https://api.twitter.com')
+      @access_token =  OAuth::AccessToken.new(
+          consumer,
+          cred['access_token'],
+          cred['access_token_secret'])
+    end
+  end
+end
